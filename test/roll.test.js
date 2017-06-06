46848ce5d41ed2a201f0ac352660287068f779f8
BuildTools <unconfigured@null.spigotmc.org>
2017-06-05 23:31:18 -0400

    Added a few more tests

--- test/roll.test.js
+++ test/roll.test.js
@@ -1,7 +1,33 @@
 import RollCommand from '../commands/random/roll';
+var assert = require('chai').assert;
 
 describe("RollCommand", () => {
     it('constructs properly', () => {
         new RollCommand({});
+    });
+
+    it('run should be a function', () => {
+      var rollCommand = new RollCommand({});
+      assert.typeOf(rollCommand.run, 'function');
+    });
+
+    it('run should return the message text', () => {
+      var rollCommand = new RollCommand({});
+      var text = "Test";
+      var message = {
+        content: text,
+        author: {
+          username: ""
+        },
+        channel: {
+          send: function(){}
+        }
+      }
+
+      var promise = rollCommand.run(message, {});
+      promise.then( (msg) => {
+        var result = msg;
+        assert.equal(result,text);
+      });
     });
 });
