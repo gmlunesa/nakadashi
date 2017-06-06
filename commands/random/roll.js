13b58a273b1d02c9a436ab65821a6c46a3045ec4
BuildTools <steven.greens10@gmail.com>
2017-06-06 14:23:51 -0400

    Updated tests

--- commands/random/roll.js
+++ commands/random/roll.js
@@ -11,21 +11,18 @@ class RollCommand extends commando.Command {
 	}
 
 	async run(message, args) {
-		console.log(message.author.username);
-
 		var splitMessage = (message.content).split(' ');
 		var max = 0;
 
 		if (splitMessage.length == 1) {
 			max = 6;
 		} else {
-			console.log(splitMessage[1]);
 			max = parseInt(splitMessage[1]);
 		}
 
 		var roll = Math.floor(Math.random() * max) + 1;
 		message.channel.send(":thinking: **" + message.author.username + "**" +  " you rolled a " + roll);
-		return message.content;
+		return roll;
 	}
 }
 
--- test/roll.test.js
+++ test/roll.test.js
@@ -11,9 +11,9 @@ describe("RollCommand", () => {
       assert.typeOf(rollCommand.run, 'function');
     });
 
-    it('run should return the message text', () => {
+    it('should roll a number between 0 and 6 by default', () => {
       var rollCommand = new RollCommand({});
-      var text = "Test";
+      var text = "!roll";
       var message = {
         content: text,
         author: {
@@ -25,9 +25,30 @@ describe("RollCommand", () => {
       }
 
       var promise = rollCommand.run(message, {});
-      return promise.then( (msg) => {
-        var result = msg;
-        assert.equal(result,text);
+      return promise.then( (roll) => {
+        var result = roll;
+        assert.isAtMost(result,6);
+      });
+    });
+
+    it('should roll a number beween 0 and first argument', () => {
+      var rollCommand = new RollCommand({});
+      var max =  10;
+      var text = "!roll " + max;
+      var message = {
+        content: text,
+        author: {
+          username: ""
+        },
+        channel: {
+          send: function(){}
+        }
+      }
+
+      var promise = rollCommand.run(message, {});
+      return promise.then( (roll) => {
+        var result = roll;
+        assert.isAtMost(result,max);
       });
     });
 });
