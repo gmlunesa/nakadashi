9fcd0e6ec65270bdd0c4538db383656ef0040f67
BuildTools <steven.greens10@gmail.com>
2017-06-06 09:07:03 -0400

    Added config for bot token

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  TOKEN: "INSERT BOT TOKEN"
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const commando = require('discord.js-commando');
 const bot = new commando.Client();
+const config = require('./config.js');
 
 /*bot.on('message', (message) => {
 	if (message.content == 'ping') {
@@ -12,6 +13,6 @@ bot.registry.registerGroup('random', 'Random');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.login('MzIwNzg2MTQ5MjI2NTc3OTIw.DBUi8Q.XOrERW_rMl1WNWkgCtfslUiKr30');
+bot.login(config.TOKEN);
 
 console.log('Bot successfully running');
