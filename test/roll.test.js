c0714a39a620add2a88bf9e08fa577dff1ad94b6
BuildTools <steven.greens10@gmail.com>
2017-06-06 16:22:06 -0400

    Update tests

--- test/roll.test.js
+++ test/roll.test.js
@@ -2,17 +2,13 @@ import RollCommand from '../commands/random/roll';
 var assert = require('chai').assert;
 
 describe("RollCommand", () => {
-    it('constructs properly', () => {
-        new RollCommand({});
-    });
+    var rollCommand = new RollCommand({});
 
     it('run should be a function', () => {
-      var rollCommand = new RollCommand({});
       assert.typeOf(rollCommand.run, 'function');
     });
 
     it('should roll a number between 0 and 6 by default', () => {
-      var rollCommand = new RollCommand({});
       var text = "!roll";
       var message = {
         content: text,
@@ -32,7 +28,6 @@ describe("RollCommand", () => {
     });
 
     it('should roll a number beween 0 and first argument', () => {
-      var rollCommand = new RollCommand({});
       var max =  10;
       var text = "!roll " + max;
       var message = {
@@ -53,7 +48,6 @@ describe("RollCommand", () => {
     });
 
     it('should roll 0 when passed 0 as argument', () => {
-      var rollCommand = new RollCommand({});
       var text = "!roll 0";
       var message = {
         content: text,
@@ -73,8 +67,26 @@ describe("RollCommand", () => {
     });
 
     it('should roll 0 when passed argument less than 0', () => {
-      var rollCommand = new RollCommand({});
       var text = "!roll -5";
+      var message = {
+        content: text,
+        author: {
+          username: ""
+        },
+        channel: {
+          send: function(){}
+        }
+      }
+
+      var promise = rollCommand.run(message, {});
+      return promise.then( (roll) => {
+        var result = roll;
+        assert.equal(result,0);
+      });
+    });
+
+    it('should roll 0 when given non-number argument', () => {
+      var text = "!roll string";
       var message = {
         content: text,
         author: {
