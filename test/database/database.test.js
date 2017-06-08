bf31662a6b667bd3e4cd415e47169a071bf166b4
BuildTools <steven.greens10@gmail.com>
2017-06-08 18:33:43 -0400

    Fix database testing

--- test/database/database.test.js
+++ test/database/database.test.js
@@ -9,17 +9,17 @@ describe('Database', function(){
   database.connect();
 
   it('Database sets and gets credits properly', function(){
-    return database.setCredits(123,100).then(function(){
-      return database.getCredits(123).then(function(credits){
+    return database.setCredits(1,100).then(function(){
+      return database.getCredits(1).then(function(credits){
         assert.equal(credits,100);
       });
     });
   });
 
   it('Database adds credits properly', function(){
-    return database.setCredits(123,0).then(function(){
-      return database.addCredits(123,10).then(function(){
-        return database.getCredits(123).then(function(credits){
+    return database.setCredits(2,0).then(function(){
+      return database.addCredits(2,10).then(function(){
+        return database.getCredits(2).then(function(credits){
           assert.equal(credits,10);
         });
       });
@@ -27,10 +27,20 @@ describe('Database', function(){
   });
 
   it('Database removes credits properly', function(){
-    return database.setCredits(123,69).then(function(){
-      return database.removeCredits(123,30).then(function(){
-        return database.getCredits(123).then(function(credits){
+    return database.setCredits(3,69).then(function(){
+      return database.removeCredits(3,30).then(function(){
+        return database.getCredits(3).then(function(credits){
           assert.equal(credits,39);
+        });
+      });
+    });
+  });
+
+  it('Database parses integers from strings', function(){
+    return database.setCredits(4,'10').then(function(){
+      return database.addCredits(4,'30').then(function(){
+        return database.getCredits(4).then(function(credits){
+          assert.equal(credits,40);
         });
       });
     });
